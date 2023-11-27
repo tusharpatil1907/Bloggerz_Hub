97bc2979a6150c36d0e577d55db82b650b30cda4
Tushar patil <tp36213@gmail.com>
2023-11-27 22:53:18 +0530

    .

--- dj_bloger/dj_bloger/settings.py
+++ dj_bloger/dj_bloger/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_x&pr39n8pd0%=&kq4*cw)*(@fhg)&6p=432w13^ilc1f*c680'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
